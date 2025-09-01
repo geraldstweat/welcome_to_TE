f37a8a409e4e0302e9462193860bba824de426af
geraldstweat <geraldstweat@proton.me>
2025-09-01 09:22:35 -0700

    Update task 1

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,47 +1,76 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 type IUser = {
-    name: string
-    age: number
-}
+    name: string;
+    age: number;
+};
 
 type IProps = {
-    user: IUser
-}
+    user: IUser;
+};
 
-// functional component
-const FirstComponent = ({ name, age }: IUser) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+// =======================
+// Functional Components
+// =======================
 
-// functional component
-// Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+// 1. FirstComponent: simple primitive props
+export const FirstComponent = React.memo(({ name, age }: IUser) => {
+    console.log('Rendering FirstComponent'); // optional: for debug
+    return (
+        <div>
+            my name is {name}, my age is {age}
+        </div>
+    );
+});
 
-// class component
-class ThirdComponent extends Component<IUser> {
-    render() {
+// 2. SecondComponent: receives an object prop
+export const SecondComponent = React.memo(
+    ({ user: { name, age } }: IProps) => {
+        console.log('Rendering SecondComponent'); // optional: for debug
         return (
             <div>
-                my name is {this.props.name}, my age is {this.props.age}
+                my name is {name}, my age is {age}
             </div>
-        )
+        );
+    },
+    (prevProps, nextProps) =>
+        prevProps.user.name === nextProps.user.name &&
+        prevProps.user.age === nextProps.user.age
+);
+
+// =======================
+// Class Components
+// =======================
+
+// 3. ThirdComponent: props are primitives
+export class ThirdComponent extends React.PureComponent<IUser> {
+    render() {
+        console.log('Rendering ThirdComponent'); // optional: for debug
+        const { name, age } = this.props;
+        return (
+            <div>
+                my name is {name}, my age is {age}
+            </div>
+        );
     }
 }
 
-// class component
-class FourthComponent extends Component<IProps> {
+// 4. FourthComponent: props is an object
+export class FourthComponent extends Component<IProps> {
+    shouldComponentUpdate(nextProps: IProps) {
+        const { user } = this.props;
+        const { user: nextUser } = nextProps;
+
+        return user.name !== nextUser.name || user.age !== nextUser.age;
+    }
+
     render() {
+        console.log('Rendering FourthComponent'); // optional: for debug
+        const { user } = this.props;
         return (
             <div>
-                my name is {this.props.user.name}, my age is {this.props.user.age}
+                my name is {user.name}, my age is {user.age}
             </div>
-        )
+        );
     }
 }
